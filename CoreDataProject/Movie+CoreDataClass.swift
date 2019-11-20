799c97d769f97b67e79cc04128fca62dffb1cc1b
Issac Penn <pbbsignup@outlook.com>
2019-11-20 12:21:39 +0800

    Day 57 done

---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by PBB on 2019/11/20.
+//  Copyright © 2019 PBB. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
